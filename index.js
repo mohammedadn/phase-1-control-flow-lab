c167d0354b3416aa61a9d71ed401fc88b21b0278
Mohammedadn <mohammedaden023@gmail.com>
2024-03-19 12:10:56 +0300

    control

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+const feet = 200 
+function scuberGreetingForFeet(feet){
+   if(feet <400){
+    return 'This one is on me!'
+   }else if(feet<= 2000 ){
+    return ('That will be twenty bucks.')
+   }else if(feet > 2000 && feet < 2500){
+    return ("I will gladly take your thirty bucks.")
+   }else {
+    return("No can do.")
+   }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
+  
+function ternaryCheckCity(city){
+   if(city == "NYC"){
+    return('Ok, sounds good.')
+ }  else{
+  return('No go.')
+ }}
+  
+  function switchOnCharmFromTip(tip){
+   switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+      case 'not as generous':
+        return'Thank you.'
+
+    default: 
+      return 'Bye.'
+      
+   }
+  }
+
+
+
+  // if(tip == "generous"){
+  //   return('Thank you so much.')
+  // } else if(tip != 'generous'){
+  //   return('Thank you.')
+  // }else{
+  //   return('Bye.')
+  // }
+
+
+
+  
+
+
+  
+  
+  
+  
